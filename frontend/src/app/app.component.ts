eeb4fdf7fa7ed381784d140f44fa2353033aa662
loico square <loicsan07@gmail.com>
2022-06-25 05:22:35 +0100

    Initialize Frontend and consume service.

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { DataState } from './enum/data-state.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { ServerService } from './service/server.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'frontend';
+  appState$: Observable<AppState<CustomResponse>>; //to capture the entire state of the application. All our objects must be of this type
+
+  constructor(private serverService: ServerService) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$.pipe(
+      map((response) => {
+        return {
+          dataState: DataState.LOADED_STATE,
+          appData: {
+            ...response,
+            data: { servers: response.data.servers.reverse() },
+          },
+        };
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
+}
